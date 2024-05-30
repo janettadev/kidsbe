9607a675c91cd0a1de7fc047a85d390360b88389
janettadev <kumarbekovajanetta@gmail.com>
2024-05-30 16:34:29 +0600

    section2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.scss";
+import Open from "./components/section#2/Open";
 
 function App() {
 	return (
 		<div className="App">
+			<Open/>
 		</div>
 	);
 }
+
 
 export default App;
