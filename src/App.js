fbf07cc5a43f2ed9623800708f66de857091488b
FatimaDEV <shamudinovafatima@gmail.com>
2024-06-06 15:46:33 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.scss";
 import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
 import Kids from "./components/kids/kids";
 import Section5 from "./components/section5/Section5";
 import Section6 from "./components/section6/section6";
@@ -8,6 +10,8 @@ import Section7 from "./components/section7/section";
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Hero />
       <Section5 />
       <Section6 />
       <Section7 />
